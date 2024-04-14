53a81b0b165c69fbc06ba6eedc24b7fc81cb0ed7
KennethMinn <minthukha2472005@gmail.com>
2024-04-14 19:43:02 +0630

    2 refrences relation

--- script.ts
+++ script.ts
@@ -2,12 +2,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: "Kenneth",
-    },
-  });
-  console.log(user);
+  //create user
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: "Kenneth",
+  //   },
+  // });
+
+  //get users
+  const users = await prisma.user.findMany();
+  console.log(users);
 }
 
 main()
